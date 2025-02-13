55e2268688121cf85eafe89c5e23bf50b4151a23
isa-pimentel <isa.deverspimentel@gmail.com>
2025-02-13 00:48:08 -0500

    Update game.js
    delete losing odds because i want them to always get something

--- game.js
+++ game.js
@@ -18,8 +18,17 @@ var xTransf = 0;
 // You can set up the odds for specific prizes here, you just need to make sure that all the odds together sum to exactly 1
 const RARE_ODDS =.005
 const COMMON_ODDS = .1
-const LOSING_ODDS = 1 - (RARE_ODDS + (COMMON_ODDS*7)) // we multiply by 7 because there are 7 prizes with common odds
-var winningOdds = {0:RARE_ODDS,1:COMMON_ODDS,2:COMMON_ODDS,3:COMMON_ODDS,4:COMMON_ODDS,5:COMMON_ODDS,6:COMMON_ODDS,7:COMMON_ODDS,8:.295}
+
+var winningOdds = {
+    0: RARE_ODDS,      // Golden ticket
+    1: COMMON_ODDS,    // Regular prizes
+    2: COMMON_ODDS,
+    3: COMMON_ODDS,
+    4: COMMON_ODDS,
+    5: COMMON_ODDS,
+    6: COMMON_ODDS,
+    7: COMMON_ODDS
+}
 
 // this tells the claw how to move when someone presses the right arrow button on the page
 right_arrow_el.addEventListener("click", (e) => {
